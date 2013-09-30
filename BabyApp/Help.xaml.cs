9ad74cbb489e1f1e5eb29d4152d6dc7db7fdd96f
Mark Senerth <Mark.Senerth@yahoo.com>
2013-09-30 07:14:10 -0400

    fixed up help screen

--- BabyApp/Help.xaml.cs
+++ BabyApp/Help.xaml.cs
@@ -7,14 +7,47 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.ComponentModel;
 
 namespace BabyApp
 {
-    public partial class Help : PhoneApplicationPage
+    public partial class Help : PhoneApplicationPage, INotifyPropertyChanged
     {
         public Help()
         {
             InitializeComponent();
+            DataContext = this;
+            PhoneWidth = Application.Current.RootVisual.RenderSize.Width;
+            PhoneHeight = Application.Current.RootVisual.RenderSize.Height;
         }
+
+
+        #region properties
+
+        private double _phoneWidth;
+        public double PhoneWidth
+        {
+            get { return _phoneWidth; }
+            set { _phoneWidth = value; RaisePropertyChanged("PhoneWidth"); }
+        }
+
+        private double _phoneHeight;
+        public double PhoneHeight
+        {
+            get { return _phoneHeight; }
+            set { _phoneHeight = value; RaisePropertyChanged("PhoneHeight"); }
+        }
+        
+
+        #endregion properties
+
+        public void RaisePropertyChanged(string t)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(t));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
